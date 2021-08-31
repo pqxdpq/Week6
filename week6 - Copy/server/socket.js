0a63b12c37c82c49235bf5ac94a880521903c86b
pqxdpq <53891628+pqxdpq@users.noreply.github.com>
2021-09-01 01:27:50 +1000

    week6copy

---
+++ week6 - Copy/server/socket.js
@@ -0,0 +1,12 @@
+module.exports = {
+    connect: function(io, PORT) {
+        io.on('connection', (socket) => {
+        // When a connection request comes in output to the server console
+        console.log('user connection on port '+ PORT + ' : '+ socket.id);
+        // when a message comes in emit it back to all sockets with the message.
+        socket.on('message',(message)=>{
+            io.emit('message', message);
+            })
+        });
+    }
+}
