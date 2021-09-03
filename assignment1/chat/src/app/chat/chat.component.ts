847b2f44b85fdf678bd333ed277f5bdef628c267
pqxdpq <53891628+pqxdpq@users.noreply.github.com>
2021-09-03 23:16:45 +1000

    show groups and rooms
    read the accessible groups of current user, and show rooms according to clicked group.

--- assignment1/chat/src/app/chat/chat.component.ts
+++ assignment1/chat/src/app/chat/chat.component.ts
@@ -13,15 +13,90 @@ export class ChatComponent implements OnInit {
     messages:string[] = [];
     ioConnection:any;
     username = sessionStorage.getItem('username');
-    role = sessionStorage.getItem('role');
+    //role = sessionStorage.getItem('role');
+    groups: string[] = [];
+    rooms: string[] = [];
     
+
+    //group array
+    groupArray: Array<any>[] = [
+      ['Group aus', 1],
+      ['Apex',2],
+      ['Hunting',3],
+      ['Basketball', 4]
+    ];
+
+    //room array
+    roomArray: Array<any>[] = [
+      ['Group aus','Animals', 5],
+      ['Group aus','Plants', 6],
+      ['Apex','aiming',7],
+      ['Apex','patch_notes',8],
+      ['Hunting','places',9],
+      ['Basketball','players', 10],
+      ['Basketball','game', 11],
+      ['Basketball','stream', 12]
+    ];
+
+    //A list of which users are in each group and rooms. 
+    authArray: Array<Array<any>[]> = [
+      [['sadmin'],[1,2,3,4,5,6,7,8,9,10,11,12]],
+      [['gadmin'],[1,2,3,4,5,6,7,8,9,10,11,12]],
+      [['aadmin'],[1,2,3,4,5,6,7,8,9,10,11,12]],
+      [['user1'],[1,2,3,4,5,6,7,8,9,10,11,12]],
+      [['user2'],[1,2,3,4,5,6,7,8,9,10,11,12]],
+      [['user3'],[1,2,3,4,5,6,7,8,9,10,11,12]]
+    ];
     
   constructor(private socketService:SocketService) { }
 
   ngOnInit() {
     this.initIoConnection();
+    console.log(this.username);
+    this.displaygroup();
+    localStorage.setItem('groups', JSON.stringify(this.groups));
+    console.log(this.groups);
   }
-  
+
+  private displaygroup(){
+    var uservar;
+    for(let i in this.authArray){
+      //find the accessable groups and rooms for current user
+      if (this.username = this.authArray[i][0][0]){
+        uservar = i;
+      }
+    }
+    for(let x in this.authArray[uservar][1]){
+      for(let y in this.groupArray){
+        if(this.authArray[uservar][1][x] == this.groupArray[y][1]){
+          this.groups.push(this.groupArray[y][0]);
+          }
+        }         
+      }
+    }
+
+  private displayroom(groupname){
+    this.rooms.length = 0;
+    var uservar;
+    console.log(groupname);
+    for(let i in this.authArray){
+      //find the accessable groups and rooms for current user
+      if (this.username = this.authArray[i][0][0]){
+        uservar = i;
+      }
+    }
+    for(let x in this.authArray[uservar][1]){
+      for(let y in this.roomArray){
+        if(this.authArray[uservar][1][x] == this.roomArray[y][2]){
+          if(groupname == this.roomArray[y][0]){
+              this.rooms.push(this.roomArray[y][1]);
+            }
+          }
+        }         
+      }
+      console.log(this.rooms);
+    }
+
   private initIoConnection(){
   this.socketService.initSocket();
   this.ioConnection = this.socketService.onMessage()
